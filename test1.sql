9f7dff83120394b153291c98ef6a745192fa83a7
HabibaAtique000 <72003391+HabibaAtique000@users.noreply.github.com>
2023-12-10 22:54:30 +0500

    Add files via upload

---
+++ test1.sql
@@ -0,0 +1,17 @@
+
+
+-- Drop the Service table
+DROP TABLE Service;
+
+-- Drop the Stop table
+DROP TABLE Stop;
+
+-- Drop the Agent table
+DROP TABLE Agent;
+
+-- Drop the Route table
+DROP TABLE Route;
+
+-- Drop the Route table
+drop table Client;
+
